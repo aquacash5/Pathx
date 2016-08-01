5380387fb5cc079b027cd87c088107164dec6c10
aquacash5 <kylejbloom@gmail.com>
2016-08-01 10:00:12 -0700

    Added __main__ check

--- pathx.py
+++ pathx.py
@@ -27,31 +27,32 @@ def directory(string):
         return string
     raise ArgumentTypeError('Invalid Directory')
 
-parser = ArgumentParser(prog='pathx')
-parser.add_argument('action',
-                    type=action,
-                    help='action to preform on %%PATH%% (APPEND, PREPEND, REMOVE)')
-parser.add_argument('pathname',
-                    type=directory,
-                    help='pathname to add to or remove from %%PATH%%')
-parser.add_argument('-S', '--system',
-                    action='store_true',
-                    help='preform action on system %%PATH%% instead of user %%PATH%%')
-args = parser.parse_args()
-registry_location = HKEY_CURRENT_USER
-sub_key = 'Environment'
-if args.system:
-    registry_location = HKEY_LOCAL_MACHINE
-    sub_key = 'SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment'
-with OpenKey(registry_location, sub_key, access=KEY_ALL_ACCESS) as environment_key:
-    path_value_list = QueryValueEx(environment_key, 'Path')[0].split(';')
-    try:
-        path_value_list.remove(args.pathname)
-    except Exception:
-        pass
-    finally:
-        if args.action == 'APPEND':
-            path_value_list.append(args.pathname)
-        elif args.action == 'PREPEND':
-            path_value_list = [args.pathname] + path_value_list
-    SetValueEx(environment_key, 'Path', 0, REG_SZ, ';'.join(path_value_list))
+if __name__ == '__main__':
+    parser = ArgumentParser(prog='pathx')
+    parser.add_argument('action',
+                        type=action,
+                        help='action to preform on %%PATH%% (APPEND, PREPEND, REMOVE)')
+    parser.add_argument('pathname',
+                        type=directory,
+                        help='pathname to add to or remove from %%PATH%%')
+    parser.add_argument('-S', '--system',
+                        action='store_true',
+                        help='preform action on system %%PATH%% instead of user %%PATH%%')
+    args = parser.parse_args()
+    registry_location = HKEY_CURRENT_USER
+    sub_key = 'Environment'
+    if args.system:
+        registry_location = HKEY_LOCAL_MACHINE
+        sub_key = 'SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment'
+    with OpenKey(registry_location, sub_key, access=KEY_ALL_ACCESS) as environment_key:
+        path_value_list = QueryValueEx(environment_key, 'Path')[0].split(';')
+        try:
+            path_value_list.remove(args.pathname)
+        except Exception:
+            pass
+        finally:
+            if args.action == 'APPEND':
+                path_value_list.append(args.pathname)
+            elif args.action == 'PREPEND':
+                path_value_list = [args.pathname] + path_value_list
+        SetValueEx(environment_key, 'Path', 0, REG_SZ, ';'.join(path_value_list))
